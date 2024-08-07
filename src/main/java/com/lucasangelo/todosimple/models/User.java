707ccee7c2be820c5dc70e590327916490b7ebfb
Matheus Queiroz <matheus.qbp@gmail.com>
2024-08-07 09:45:56 -0300

    Create UserController

--- src/main/java/com/lucasangelo/todosimple/models/User.java
+++ src/main/java/com/lucasangelo/todosimple/models/User.java
@@ -11,6 +11,7 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
@@ -79,7 +80,7 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
-
+    @JsonIgnore
     public List<Task> getTasks() {
         return this.tasks;
     }
